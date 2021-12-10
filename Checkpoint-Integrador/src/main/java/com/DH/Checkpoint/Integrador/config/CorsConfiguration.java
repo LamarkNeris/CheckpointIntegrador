c719bce131915027731493fc7571e4e84f6ab2ac
Lamark <lamark12.neris@gmail.com>
2021-12-10 12:10:04 -0300

    final

--- Checkpoint-Integrador/src/main/java/com/DH/Checkpoint/Integrador/config/CorsConfiguration.java
+++ Checkpoint-Integrador/src/main/java/com/DH/Checkpoint/Integrador/config/CorsConfiguration.java
@@ -6,19 +6,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //liberando app cliente 1
+        registry.addMapping("/**")
+                .allowedOrigins("https://ctdiii-checkpoint-final.vercel.app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
 
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            //liberando app cliente 1
-            registry.addMapping("/**")
-                    .allowedOrigins("https://ctdiii-checkpoint-final.vercel.app/")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-
-            //liberando app cliente 2
-            registry.addMapping("/topicos/**")
-                    .allowedOrigins("http://localhost:4000")
-                    .allowedMethods("GET", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-        }
+        //liberando app cliente 2
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
+}
 
 
